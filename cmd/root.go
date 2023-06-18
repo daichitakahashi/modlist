a6d6a0f3a9a10ce295f23a0147cfec56a1802e90
daichitakahashi <t.daichi.snd@gmail.com>
2023-06-19 02:04:58 +0900

    Update help sentence

--- cmd/root.go
+++ cmd/root.go
@@ -16,13 +16,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "modlist",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List your go modules/packages.",
+	Long:  "List your go modules/packages.",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	RunE: func(cmd *cobra.Command, args []string) error {
